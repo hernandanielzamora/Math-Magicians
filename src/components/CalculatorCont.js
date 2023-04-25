27daf4d7ac81e476d1bef9b61f162d3e1f88d83d
Hernan Zamora <hernanzamora1002@gmail.com>
2023-04-25 13:08:52 -0300

    Created calculator components (buttons) and structure

---
+++ src/components/CalculatorCont.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CalculatorComp from './Calculator';
+import '../App.css';
+
+function CalculatorCont() {
+  return (
+    <>
+      <div className="calculator-container">
+        <CalculatorComp />
+      </div>
+    </>
+  );
+}
+
+export default CalculatorCont;
