7f6d344b496ba560c60aadf573ff7d497d6182c2
Hernan Zamora <hernanzamora1002@gmail.com>
2023-04-26 18:28:56 -0300

    Created quote divs, fetched the data from Ninja API, created styles for the quote and the author

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quotes from './components/Quotes';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="calculator-container">
         <Calculator />
       </div>
+      <Quotes />
     </div>
   );
 }
